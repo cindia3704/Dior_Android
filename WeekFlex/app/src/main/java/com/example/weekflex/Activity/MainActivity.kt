c2b5fa88c74c178876f8d1e8870d0ff5f84356bc
cindia3704 <cindia3704@gmail.com>
2021-01-24 01:08:46 +0900

    feat: basic settings

---
+++ WeekFlex/app/src/main/java/com/example/weekflex/Activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weekflex.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weekflex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
