13d66d81ebdbddad8a89a0804db5e8b676fd7526
Jisoo Kim <52744390+cindia3704@users.noreply.github.com>
2021-02-07 21:40:43 +0900

    feat: make and connect all fragments (#5)

---
+++ WeekFlex/app/src/main/java/com/example/weekflex/Adapter/NavigationTabAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.weekflex.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.weekflex.Fragment.CalendarFragment
+import com.example.weekflex.Fragment.RoutineSettingFragment
+import com.example.weekflex.Fragment.TodoMainFragment
+
+class NavigationTabAdapter (fragmentManager: FragmentManager, val fragmentCount:Int,val userId:Int) : FragmentStatePagerAdapter(fragmentManager){
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0-> return TodoMainFragment.newInstance(userId)
+            1-> return CalendarFragment.newInstance(userId)
+            else -> return RoutineSettingFragment.newInstance(userId)
+        }
+    }
+
+    override fun getCount(): Int {
+        return fragmentCount
+    }
+}
