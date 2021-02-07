7241fab15b4e24ce5cde29644b61167eed953c82
cindia3704 <cindia3704@gmail.com>
2021-02-07 20:39:28 +0900

    feat: make and connect all fragments

--- WeekFlex/app/src/main/java/com/example/weekflex/Activity/MainActivity.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Activity/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.weekflex.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.viewpager.widget.ViewPager
+import com.example.weekflex.Adapter.NavigationTabAdapter
 import com.example.weekflex.R
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    var userId: Int =1
+    private lateinit var fragmentView : ViewPager
+    private lateinit var navigationBar: TabLayout
+    private lateinit var fragmentAdapter: NavigationTabAdapter
+    private lateinit var navigationBarLayout: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        makeNavigationBar()
     }
+
+    fun initView(){
+        fragmentView = findViewById(R.id.fragmentViewMain)
+        navigationBar =findViewById(R.id.navigationBar)
+        fragmentAdapter = NavigationTabAdapter(supportFragmentManager,3,userId)
+        navigationBarLayout = this.layoutInflater.inflate(R.layout.navibation_bar,null,false)
+        fragmentView.adapter=fragmentAdapter
+        navigationBar.setupWithViewPager(fragmentView)
+        fragmentView.currentItem=0
+    }
+
+    private fun makeNavigationBar(){
+        navigationBar.getTabAt(0)!!.customView= navigationBarLayout.findViewById(R.id.mainTab) as RelativeLayout
+        navigationBar.getTabAt(1)!!.customView = navigationBarLayout.findViewById(R.id.calendarTab)as RelativeLayout
+        navigationBar.getTabAt(2)!!.customView = navigationBarLayout.findViewById(R.id.routineTab)as RelativeLayout
+    }
+
+
+
 }
---
+++ WeekFlex/app/src/main/java/com/example/weekflex/Fragment/TodoMainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.weekflex.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weekflex.R
+
+class TodoMainFragment : Fragment(){
+    private var userId : Int? =1
+    companion object{
+        fun newInstance(userId:Int):TodoMainFragment{
+            var bundle=Bundle()
+            bundle.putInt("userId",userId)
+            var fragment = TodoMainFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        userId = arguments?.getInt("userId")
+        return inflater.inflate(R.layout.todo_main_fragment,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
