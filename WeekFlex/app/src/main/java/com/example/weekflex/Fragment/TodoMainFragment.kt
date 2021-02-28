fc611b3b059a56094b360a32f4422af9429fe426
cindia3704 <cindia3704@gmail.com>
2021-02-28 14:34:19 +0900

    feat: apply design to 1 week calendar view

--- WeekFlex/app/src/main/java/com/example/weekflex/Fragment/TodoMainFragment.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Fragment/TodoMainFragment.kt
@@ -2,15 +2,11 @@ package com.example.weekflex.Fragment
 
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.weekflex.Activity.AddRoutineActivity
 import com.example.weekflex.Activity.AddTodoActivity
@@ -18,18 +14,14 @@ import com.example.weekflex.Adapter.TodoWeekDateAdapter
 import com.example.weekflex.R
 import kotlinx.android.synthetic.main.todo_main_fragment.*
 import java.text.SimpleDateFormat
-import java.time.DayOfWeek
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.Month
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 class TodoMainFragment : Fragment(){
     private var userId : Int? =1
     private var calendar= Calendar.getInstance()
     private var addClicked = false
-    private var weekOfDay=arrayOf("일요일","월요일","화요일","수요일","목요일","금요일","토요일")
+    private var weekOfDayEnglish=arrayOf("Sunday","Tuesday","Wednesday","Thursday","Friday","Saturday","Monday")
+    private var weekOfDay=arrayOf("월요일","화요일","수요일","목요일","금요일","토요일","일요일")
     private var dfDate : SimpleDateFormat=  SimpleDateFormat("yyyy-MM-dd",Locale.KOREA)
 //    var weekHeaderAdapter = TodoWeekDateAdapter(calendar.get(Calendar.DATE),calendar.get(Calendar.DAY_OF_WEEK),weekOfDay,this)
     companion object{
@@ -60,8 +52,12 @@ class TodoMainFragment : Fragment(){
     fun initView(){
         val dateToday = getTodayDate()
         dateToday_todo.setText(dateToday)
+        val day = calendar.get(Calendar.DATE)
         val days = getThisWeek()
-        weekHeader_recylerview.adapter =TodoWeekDateAdapter(days,weekOfDay,this)
+        Log.d("msg","오늘: "+day.toString())
+        weekHeader_recylerview.setHasFixedSize(true)
+        weekHeader_recylerview.adapter =TodoWeekDateAdapter(days,weekOfDay,this,day.toString())
+        weekHeader_recylerview.addItemDecoration(RecyclerDecoration(100))
     }
 
     fun setListener(){
@@ -94,13 +90,14 @@ class TodoMainFragment : Fragment(){
         Log.d("msg","월: "+month)
         Log.d("msg","일: "+day)
         Log.d("msg","요일: "+dayOfWeek)
-        val todayDate=""+(month+1)+"월 "+day+"일, "+weekOfDay[(dayOfWeek-1)]
+        val todayDate=""+(month+1)+"월 "+day+"일, "+weekOfDayEnglish[(dayOfWeek-1)]
         return todayDate
     }
 
     private fun getThisWeek(): Array<Any> {
         var thisWeek = ArrayList<String>(7)
-        var todayDay = calendar.get(Calendar.DAY_OF_WEEK)-1
+        var todayDay = calendar.get(Calendar.DAY_OF_WEEK)+5
+        Log.d("msg","todayDay: "+todayDay.toString())
         if(todayDay != 0){
             val differenceInDay = 0-todayDay
             calendar.add(Calendar.DATE,differenceInDay)
