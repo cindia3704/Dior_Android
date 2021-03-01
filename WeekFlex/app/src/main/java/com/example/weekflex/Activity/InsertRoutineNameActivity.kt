dc04922f4876d758641e1237bc8e7ad727dcd9b1
cindia3704 <cindia3704@gmail.com>
2021-03-02 02:14:15 +0900

    feat add design for insert routine name and tab view

--- WeekFlex/app/src/main/java/com/example/weekflex/Activity/InsertRoutineNameActivity.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Activity/InsertRoutineNameActivity.kt
@@ -1,18 +1,25 @@
 package com.example.weekflex.Activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.example.weekflex.R
 
 
 class InsertRoutineNameActivity : AppCompatActivity() {
     private lateinit var backBtn : Button
     private lateinit var routineNameView: EditText
+    private lateinit var checkImg : ImageView
+    private lateinit var routineNameCommentView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert_routine_name)
@@ -24,19 +31,25 @@ class InsertRoutineNameActivity : AppCompatActivity() {
         backBtn = findViewById(R.id.back_insertRoutineName)
         routineNameView= findViewById(R.id.insertRoutineName)
         routineNameView.imeOptions = EditorInfo.IME_ACTION_DONE
-
+        checkImg = findViewById(R.id.checkImg_insertRoutineName)
+        routineNameCommentView = findViewById(R.id.addRoutineName_ment)
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
     }
 
     private fun setListener(){
         backBtn.setOnClickListener {
+            hideKeyboard()
             finish()
         }
         // 키보드 enter --> 완료로 바꾸고 다음 액티비티로 보내기
         routineNameView.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
             when (actionId) {
                 EditorInfo.IME_ACTION_DONE -> {
+                    hideKeyboard()
                     val intent = Intent(this@InsertRoutineNameActivity,CompleteMakeRoutineActivity::class.java)
                     startActivity(intent)
+                    finish()
                 }
                 else -> {
                 }
@@ -44,6 +57,21 @@ class InsertRoutineNameActivity : AppCompatActivity() {
             true
         })
 
+        //루틴 이름 입력시 코멘트 바꾸기 & 상단 체크 표시 바꾸기
+        routineNameView.doAfterTextChanged { s ->
+            if (s.isNullOrEmpty() || s.length <2) {
+               checkImg.setImageResource(R.drawable.uncheck)
+                routineNameCommentView.setText("루틴 이름을 입력해주세요")
+            } else {
+                checkImg.setImageResource(R.drawable.check)
+                routineNameCommentView.setText("멋진 이름이에요! *-*")
+            }
+        }
+    }
+
+    fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(routineNameView.windowToken, 0)
     }
 
 
--- WeekFlex/app/src/main/java/com/example/weekflex/Activity/MainActivity.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Activity/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example.weekflex.Activity
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
-import androidx.annotation.RequiresApi
-
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.example.weekflex.Adapter.NavigationTabAdapter
 import com.example.weekflex.R
 import com.google.android.material.tabs.TabLayout
-
-import java.time.LocalDate
-import java.util.*
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,11 +19,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navigationBar: TabLayout
     private lateinit var fragmentAdapter: NavigationTabAdapter
     private lateinit var navigationBarLayout: View
+    private lateinit var mainTabImg : ImageView
+    private lateinit var calendarTabImg : ImageView
+    private lateinit var routineTabImg : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
         makeNavigationBar()
+
     }
 
     fun initView(){
@@ -37,16 +36,48 @@ class MainActivity : AppCompatActivity() {
         navigationBar =findViewById(R.id.navigationBar)
         fragmentAdapter = NavigationTabAdapter(supportFragmentManager,3,userId)
         navigationBarLayout = this.layoutInflater.inflate(R.layout.navibation_bar,null,false)
+        mainTabImg = navigationBarLayout.findViewById(R.id.mainTab_img)
+        routineTabImg =  navigationBarLayout.findViewById(R.id.routineTab_img)
+        calendarTabImg =  navigationBarLayout.findViewById(R.id.calendarTab_img)
         fragmentView.adapter=fragmentAdapter
         navigationBar.setupWithViewPager(fragmentView)
         fragmentView.currentItem=0
+
     }
 
     private fun makeNavigationBar(){
         navigationBar.getTabAt(0)!!.customView= navigationBarLayout.findViewById(R.id.mainTab) as RelativeLayout
         navigationBar.getTabAt(1)!!.customView = navigationBarLayout.findViewById(R.id.calendarTab)as RelativeLayout
         navigationBar.getTabAt(2)!!.customView = navigationBarLayout.findViewById(R.id.routineTab)as RelativeLayout
+
+        // 탭 바꿀때마다 이미지 교체
+        navigationBar.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                if (tab.position == 0) {
+                    mainTabImg.setImageResource(R.drawable.home)
+                    calendarTabImg.setImageResource(R.drawable.calendar)
+                    routineTabImg.setImageResource(R.drawable.profile)
+                }
+                if (tab.position == 1) {
+                    calendarTabImg.setImageResource(R.drawable.calendarselected)
+                    mainTabImg.setImageResource(R.drawable.homeunselected)
+                    routineTabImg.setImageResource(R.drawable.profile)
+                }
+                if (tab.position == 2) {
+                    routineTabImg.setImageResource(R.drawable.profileselected)
+                    mainTabImg.setImageResource(R.drawable.homeunselected)
+                    calendarTabImg.setImageResource(R.drawable.calendar)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
+
 
 
 
