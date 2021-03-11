373e42b758f2ece472ebebdf83780856b42a58ec
pula39 <programmed_life@naver.com>
2021-03-12 00:38:54 +0900

    #19 실제 디자인의 루틴 데이터와 맟추기위해 Routine과 RoutineItem 변경. 좀더 kotlin기능을 사용해서 함수 사용하게 변경. (listOf 같은거)

--- WeekFlex/app/src/main/java/com/example/weekflex/Activity/AddRoutineActivity.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Activity/AddRoutineActivity.kt
@@ -10,9 +10,27 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weekflex.Adapter.RoutineListAdapter
 import com.example.weekflex.Data.Routine
+import com.example.weekflex.Data.RoutineItem
 import com.example.weekflex.Fragment.TodoMainFragment
 import com.example.weekflex.R
 import kotlinx.android.synthetic.main.activity_add_routine.*
+
+val routineList = listOf(
+        Routine("English Master :-)", listOf(
+                RoutineItem("스피킹", 3, "10:00AM", "1:00PM"),
+                RoutineItem("전화영어", 2, "1:00PM", "1:30PM"),
+                RoutineItem("스피킹", 1, "5:00PM", "6:00PM")
+        )),
+        Routine("빡세게 면접 준비", listOf(
+                RoutineItem("CS", 3, "10:00AM", "1:00PM"),
+                RoutineItem("알고리즘", 2, "1:00PM", "1:30PM")
+        )),
+        Routine("운동 뿌셔", listOf(
+                RoutineItem("링피트", 1, "10:00AM", "1:00PM"),
+                RoutineItem("전화영어", 2, "1:00PM", "1:30PM"),
+                RoutineItem("스피킹", 3, "5:00PM", "6:00PM")
+        ))
+)
 
 class AddRoutineActivity : AppCompatActivity() {
     private lateinit var backBtn:Button
@@ -44,18 +62,9 @@ class AddRoutineActivity : AppCompatActivity() {
 
     private fun layoutInit(){
         //TODO: 서버랑 연결해서 루틴 리스트 받아오기
-        val routineList = ArrayList<Routine>()
-        routineList.add(Routine("면접집중"))
-        routineList.add(Routine("면접집중"))
-        routineList.add(Routine("면접집중"))
-        routineList.add(Routine("면접집중"))
-        routineList.add(Routine("운동집중"))
-        routineList.add(Routine("영어공부"))
-        routineList.add(Routine("취업"))
 
-        val adapter = RoutineListAdapter(LayoutInflater.from(this@AddRoutineActivity),routineList)
-//        recyclerview_addRoutine.layoutManager= LinearLayoutManager(this@AddRoutineActivity)
+        val adapter = RoutineListAdapter(LayoutInflater.from(this@AddRoutineActivity), routineList)
         recyclerview_addRoutine.adapter = adapter
-
+        recyclerview_addRoutine.layoutManager= GridLayoutManager(this@AddRoutineActivity,1,GridLayoutManager.VERTICAL,false)
     }
 }
--- WeekFlex/app/src/main/java/com/example/weekflex/Data/Routine.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Data/Routine.kt
@@ -1,6 +1,17 @@
 package com.example.weekflex.Data
 
+import com.example.weekflex.Activity.routineList
+import com.example.weekflex.R
+import java.util.*
+
 data class Routine (
-    val routineTitle: String
-    //val routineItemList:Array<RoutineItem>
+    val routineTitle: String,
+    val routineItemList:List<RoutineItem> = emptyList()
+)
+
+val categoryToStarImage = mapOf<Int, Int>(
+        0 to R.drawable.graystar,
+        1 to R.drawable.bluestar,
+        2 to R.drawable.yellowstar,
+        3 to R.drawable.pinkstar
 )
