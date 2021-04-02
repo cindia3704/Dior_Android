63904705cfb4de402de075fa9f1f034f03624fba
pula39 <programmed_life@naver.com>
2021-04-03 01:06:52 +0900

    #25 태스크 수정하기 BottomDialog가 매끈하게 올라오게 수정
     - Animation을 사용하지않고 BottomDialogBehaivor를 사용해서 직접 조작

--- WeekFlex/app/src/main/java/com/example/weekflex/Fragment/RoutineModifyBottomDialogFragment.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Fragment/RoutineModifyBottomDialogFragment.kt
@@ -1,40 +1,49 @@
 package com.example.weekflex.Fragment
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.weekflex.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class RoutineModifyBottomDialogFragment : BottomSheetDialogFragment(), View.OnClickListener {
-    private var msgLo: LinearLayout? = null
-    private var emailLo: LinearLayout? = null
-    private var cloudLo: LinearLayout? = null
-    private var bluetoothLo: LinearLayout? = null
+    private lateinit var dialogView: View
+    private lateinit var taskNameTextView: TextView
+    private lateinit var taskModifyTextView: TextView
+    private lateinit var taskDeleteTextView: TextView
+    private lateinit var cancelImageView: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.routine_modify_bottom_dialog_layout, container, false)
-        msgLo = view.findViewById(R.id.msgLo)
-        emailLo = view.findViewById(R.id.emailLo)
-        cloudLo = view.findViewById(R.id.cloudLo)
-        bluetoothLo = view.findViewById(R.id.bluetoothLo)
-        msgLo!!.setOnClickListener(this)
-        emailLo!!.setOnClickListener(this)
-        cloudLo!!.setOnClickListener(this)
-        bluetoothLo!!.setOnClickListener(this)
+        val view = inflater.inflate(R.layout.routine_modify_bottom_dialog_layout, container, false)
+        dialogView = view
+
+        taskNameTextView = dialogView.findViewById(R.id.task_name_textView)
+
+        taskModifyTextView = dialogView.findViewById(R.id.task_modify_textview)
+        taskDeleteTextView = dialogView.findViewById(R.id.task_delete_textview)
+        taskDeleteTextView.setOnClickListener {
+            Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show()
+        }
+        cancelImageView = dialogView.findViewById(R.id.task_modify_dialog_close_imageView)
+
         return view
     }
 
     override fun onClick(view: View) {
         when (view.getId()) {
-            R.id.msgLo -> Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show()
-            R.id.emailLo -> Toast.makeText(context, "Email", Toast.LENGTH_SHORT).show()
-            R.id.cloudLo -> Toast.makeText(context, "Cloud", Toast.LENGTH_SHORT).show()
-            R.id.bluetoothLo -> Toast.makeText(context, "Bluetooth", Toast.LENGTH_SHORT).show()
+            taskModifyTextView.id -> Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show()
+            taskDeleteTextView.id -> Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show()
+            cancelImageView.id -> Toast.makeText(context, "Message", Toast.LENGTH_SHORT).show()
         }
         dismiss()
     }
@@ -46,8 +55,28 @@ class RoutineModifyBottomDialogFragment : BottomSheetDialogFragment(), View.OnCl
 
     override fun onActivityCreated(arg0: Bundle?) {
         super.onActivityCreated(arg0)
-        if(dialog != null){
-            dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog?.window?.setWindowAnimations(R.style.DialogAnimation)
+//        dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimation
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+//        dialog?.window?.setWindowAnimations(R.style.DialogAnimation)
+
+        dialog.setOnShowListener { dialog ->
+            val d = dialog as BottomSheetDialog
+            val bottomSheet =
+                d.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+            Log.d("하이","좀 자자")
+            BottomSheetBehavior.from<FrameLayout?>(bottomSheet!!).peekHeight = 0
+            BottomSheetBehavior.from<FrameLayout?>(bottomSheet!!).state = BottomSheetBehavior.STATE_COLLAPSED
+            BottomSheetBehavior.from<FrameLayout?>(bottomSheet!!).state = BottomSheetBehavior.STATE_EXPANDED
         }
+
+        // Do something with your dialog like setContentView() or whatever
+        Log.d("Dismiss", dialog.dismissWithAnimation.toString())
+        dialog.dismissWithAnimation = true
+        return dialog
     }
 }
