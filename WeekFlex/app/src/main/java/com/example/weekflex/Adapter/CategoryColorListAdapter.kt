0b38cc8ed561e65b45d4e0127879be047c3e1107
cindia3704 <cindia3704@gmail.com>
2021-04-27 23:47:58 +0900

    #35 카테고리 별 색 추가

--- WeekFlex/app/src/main/java/com/example/weekflex/Adapter/CategoryColorListAdapter.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Adapter/CategoryColorListAdapter.kt
@@ -1,2 +1,43 @@
 package com.example.weekflex.Adapter
 
+package com.example.weekflex.Adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weekflex.R
+
+class CategoryColorListAdapter(
+    val inflater: LayoutInflater,
+    val availableColors: List<Int>,
+    val selectedColor : Int
+) : RecyclerView.Adapter<CategoryColorListAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val colorCircle: ImageView
+        val checkImg: ImageView
+
+        init {
+            colorCircle = itemView.findViewById(R.id.category_color)
+            checkImg = itemView.findViewById(R.id.category_color_check_img)
+        }
+    }
+
+    // 생성될 때 해주는거
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layout = R.layout.category_colors_list_item
+        val view = inflater.inflate(layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return availableColors.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+}
--- WeekFlex/app/src/main/java/com/example/weekflex/Data/Routine.kt
+++ WeekFlex/app/src/main/java/com/example/weekflex/Data/Routine.kt
@@ -14,3 +14,21 @@ val categoryToStarImage = mapOf<Int, Int>(
         2 to R.drawable.yellowstar,
         3 to R.drawable.pinkstar
 )
+
+val categoryColors = mapOf <Int,Int>(
+    1 to R.color.color1,
+    2 to R.color.color2,
+    3 to R.color.color3,
+    4 to R.color.color4,
+    5 to R.color.color5,
+    6 to R.color.color06,
+    7 to R.color.color7,
+    8 to R.color.color8,
+    9 to R.color.color9,
+    10 to R.color.color10,
+    11 to R.color.color11,
+    12 to R.color.color12,
+    13 to R.color.color13,
+    14 to R.color.color14,
+    15 to R.color.color15
+)
